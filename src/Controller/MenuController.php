673a1c93e535631533d44bff192ade9961e998eb
ilyesblg <boualleguiilyes1@gmail.com>
2022-04-08 11:20:31 +0100

    adding files

---
+++ src/Controller/MenuController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Menu;
+use App\Form\MenuType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MenuController extends AbstractController
+{
+    /**
+     * @Route("/menu", name="display_menu")
+     */
+    public function index(): Response
+    {
+        $menus = $this->getDoctrine()->getManager()->getRepository(Menu::class)->findAll();
+        return $this->render('menu/index.html.twig', ['m' => $menus]
+        );
+    }
+
+    /**
+     * @Route("/AddMenu", name="Add_Menu")
+     */
+    public function AddMenu(Request $request): Response
+    {
+        $menu = new Menu();
+        $form = $this->createForm(MenuType::class, $menu);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($menu);
+            $em->flush();
+            return $this->redirectToRoute('display_menu');
+
+        }
+        return $this->render('menu/createmenu.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/RemoveMenu/{id}", name="Remove_Menu")
+     */
+    public function RemoveMenu(Menu $menu): Response
+    {
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($menu);
+            $em->flush();
+
+            return $this->redirectToRoute('display_menu');
+        }
+    }
+    /**
+     * @Route("/UpdateMenu/{id}", name="Update_Menu")
+     */
+    public function UpdateMenu(Request $request , $id): Response
+    {$menu = $this->getDoctrine()->getManager()->getRepository(Menu::class)->find($id);
+        $form = $this->createForm(MenuType::class, $menu);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->redirectToRoute('display_menu');
+
+        }
+        return $this->render('menu/updatemenu.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+}
---
+++ src/Entity/Inorder.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InorderRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=InorderRepository::class)
+ */
+class Inorder
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $menu_id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $order_id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $quantity;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMenuId(): ?int
+    {
+        return $this->menu_id;
+    }
+
+    public function setMenuId(int $menu_id): self
+    {
+        $this->menu_id = $menu_id;
+
+        return $this;
+    }
+
+    public function getOrderId(): ?int
+    {
+        return $this->order_id;
+    }
+
+    public function setOrderId(int $order_id): self
+    {
+        $this->order_id = $order_id;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+}
